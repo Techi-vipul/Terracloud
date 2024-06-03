a1ac1ecdcd308ccf215c6b0a88d6ae96902302f7
Techi-vipul <82360686+Techi-vipul@users.noreply.github.com>
2024-06-03 21:28:37 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 1.8.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.51.1"
+    }
+  }
+  cloud {
+    organization = "Terraform-June-Learning"
+
+    workspaces {
+      name = "Cli-driven"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+# create one aws_vpc
+
+resource "aws_vpc" "vpc01" {
+  cidr_block = "10.0.0.0/16"
+}
